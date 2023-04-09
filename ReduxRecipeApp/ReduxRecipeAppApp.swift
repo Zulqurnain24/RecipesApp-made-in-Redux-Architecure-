5b6a63e4c3f38be7d8c0703e10472cf80caa7388
Muhammad Zulqurnain <muhammad@kalido.me>
2023-04-09 12:44:26 +0100

    Initial Commit

---
+++ ReduxRecipeApp/ReduxRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxRecipeAppApp.swift
+//  ReduxRecipeApp
+//
+//  Created by Muhammad Zulqurnain on 09/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReduxRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
